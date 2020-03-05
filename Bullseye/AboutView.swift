ed7d1a87883a7f34de3aeca8df404a4a65ad6038
Yauhen  Chyrko <chirko.eugene@gmail.com>
2020-03-05 11:19:34 +0300

    initial commit

---
+++ Bullseye/AboutView.swift
@@ -0,0 +1,53 @@
+//
+//  AboutView.swift
+//  Bullseye
+//
+//  Created by malakoipechyva on 2/5/20.
+//  Copyright © 2020 malakoipechyva. All rights reserved.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    
+    let beige = Color(red: 255.0 / 255.0 , green: 214.0 / 255.0, blue: 179.0 / 255.0)
+    
+    struct AboutHeadingStyle: ViewModifier {
+           func body(content: Content) -> some View {
+               return content
+               .foregroundColor(Color.black)
+               .font(Font.custom("Arial Rounded MT Bold", size: 30))
+               .padding(.top, 20)
+               .padding(.bottom, 20)
+           }
+       }
+    struct AboutBodyStyle: ViewModifier {
+             func body(content: Content) -> some View {
+                 return content
+                 .foregroundColor(Color.black)
+                 .font(Font.custom("Arial Rounded MT Bold", size: 16))
+                 .padding(.leading, 60)
+                 .padding(.trailing, 60)
+                 .padding(.bottom, 20)
+             }
+         }
+    var body: some View {
+        Group {
+        VStack {
+            Text("🎯 Bullseye 🎯").modifier(AboutHeadingStyle())
+            Text("This is Bullseye, the game where you can win points and earn fame by dragging a slider.").modifier(AboutBodyStyle())
+            Text("Your goal is to place the slider as close is possible to the target value. The closer you are, the more point of your score.").modifier(AboutBodyStyle()).lineLimit(nil)
+            Text("Enjoy!").modifier(AboutBodyStyle())
+        }
+    .navigationBarTitle("About Bullseye")
+    .background(beige)
+      }
+    .background(Image("Background"))
+    }
+ }
+
+struct AboutView_Previews: PreviewProvider {
+    static var previews: some View {
+        AboutView().previewLayout(.fixed(width: 896, height: 414))
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+## BullsEye game from [Ray Wenderlich's courses](https://www.raywenderlich.com/4919757-your-first-ios-and-swiftui-app), SwiftUI version
